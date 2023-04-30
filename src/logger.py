166b42e40d91a6fad392ceaddd5b465d85fdbab9
Bogdan Popescu <64642962+Bogdan-C-P@users.noreply.github.com>
2023-04-30 20:14:54 +0300

    Add files via upload

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = "{}.log".format(datetime.now().strftime("%d_%m_%Y_%H_%M_%S"))
+log_file_path = os.path.join(os.getcwd(), "logs" ,LOG_FILE)
+os.makedirs(log_file_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_file_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(message)s",
+    level =  logging.INFO,
+
+)
+
+if __name__=="__main__":
+    logging.info("Logging has started")
